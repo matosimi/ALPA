27d36a397589d7e8b52c3a68caa5980d0603c86c
Martin Simecek <martin.simecek@dxc.com>
2023-09-11 14:20:14 +0200

    fixes fixes

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,11 +34,25 @@
             pictureBox2 = new PictureBox();
             pictureBox3 = new PictureBox();
             pictureBox4 = new PictureBox();
-            checkBox1 = new CheckBox();
+            checkBoxInverse = new CheckBox();
+            pictureBox5 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            pictureBox7 = new PictureBox();
+            button2 = new Button();
+            pictureBox8 = new PictureBox();
+            checkBoxSimpleAvg = new CheckBox();
+            comboBoxDither = new ComboBox();
+            checkBoxUseDither = new CheckBox();
+            comboBoxDistance = new ComboBox();
+            label1 = new Label();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
             SuspendLayout();
             // 
             // pictureBox1
@@ -46,17 +60,17 @@
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
             pictureBox1.Location = new Point(12, 12);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(160, 206);
+            pictureBox1.Size = new Size(160, 160);
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
             // 
             // button1
             // 
-            button1.Location = new Point(291, 37);
+            button1.Location = new Point(311, 12);
             button1.Name = "button1";
             button1.Size = new Size(53, 64);
             button1.TabIndex = 1;
-            button1.Text = "button1";
+            button1.Text = "Aprioximate";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
@@ -64,43 +78,150 @@
             // 
             pictureBox2.Location = new Point(370, 12);
             pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(317, 206);
+            pictureBox2.Size = new Size(256, 136);
             pictureBox2.TabIndex = 2;
             pictureBox2.TabStop = false;
             // 
             // pictureBox3
             // 
-            pictureBox3.Location = new Point(370, 238);
+            pictureBox3.Location = new Point(370, 154);
             pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(317, 206);
+            pictureBox3.Size = new Size(256, 136);
             pictureBox3.TabIndex = 3;
             pictureBox3.TabStop = false;
             // 
             // pictureBox4
             // 
-            pictureBox4.Location = new Point(12, 238);
+            pictureBox4.Location = new Point(12, 240);
             pictureBox4.Name = "pictureBox4";
-            pictureBox4.Size = new Size(293, 206);
+            pictureBox4.Size = new Size(256, 200);
             pictureBox4.TabIndex = 4;
             pictureBox4.TabStop = false;
             // 
-            // checkBox1
+            // checkBoxInverse
             // 
-            checkBox1.AutoSize = true;
-            checkBox1.Location = new Point(281, 12);
-            checkBox1.Name = "checkBox1";
-            checkBox1.Size = new Size(63, 19);
-            checkBox1.TabIndex = 5;
-            checkBox1.Text = "inverse";
-            checkBox1.UseVisualStyleBackColor = true;
-            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            checkBoxInverse.AutoSize = true;
+            checkBoxInverse.Location = new Point(178, 12);
+            checkBoxInverse.Name = "checkBoxInverse";
+            checkBoxInverse.Size = new Size(63, 19);
+            checkBoxInverse.TabIndex = 5;
+            checkBoxInverse.Text = "inverse";
+            checkBoxInverse.UseVisualStyleBackColor = true;
+            checkBoxInverse.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Location = new Point(632, 12);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(256, 136);
+            pictureBox5.TabIndex = 6;
+            pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Location = new Point(632, 154);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(256, 136);
+            pictureBox6.TabIndex = 7;
+            pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.Location = new Point(632, 296);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(256, 136);
+            pictureBox7.TabIndex = 8;
+            pictureBox7.TabStop = false;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(311, 84);
+            button2.Name = "button2";
+            button2.Size = new Size(53, 64);
+            button2.TabIndex = 9;
+            button2.Text = "Mix";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // pictureBox8
+            // 
+            pictureBox8.Location = new Point(370, 296);
+            pictureBox8.Name = "pictureBox8";
+            pictureBox8.Size = new Size(256, 136);
+            pictureBox8.TabIndex = 10;
+            pictureBox8.TabStop = false;
+            // 
+            // checkBoxSimpleAvg
+            // 
+            checkBoxSimpleAvg.AutoSize = true;
+            checkBoxSimpleAvg.Checked = true;
+            checkBoxSimpleAvg.CheckState = CheckState.Checked;
+            checkBoxSimpleAvg.Location = new Point(178, 37);
+            checkBoxSimpleAvg.Name = "checkBoxSimpleAvg";
+            checkBoxSimpleAvg.Size = new Size(117, 19);
+            checkBoxSimpleAvg.TabIndex = 11;
+            checkBoxSimpleAvg.Text = "Simple averaging";
+            checkBoxSimpleAvg.UseVisualStyleBackColor = true;
+            checkBoxSimpleAvg.CheckedChanged += checkBoxSimple_CheckedChanged;
+            // 
+            // comboBoxDither
+            // 
+            comboBoxDither.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBoxDither.FormattingEnabled = true;
+            comboBoxDither.Items.AddRange(new object[] { "chess", "sierra", "F-S" });
+            comboBoxDither.Location = new Point(176, 87);
+            comboBoxDither.Name = "comboBoxDither";
+            comboBoxDither.Size = new Size(96, 23);
+            comboBoxDither.TabIndex = 13;
+            // 
+            // checkBoxUseDither
+            // 
+            checkBoxUseDither.AutoSize = true;
+            checkBoxUseDither.Checked = true;
+            checkBoxUseDither.CheckState = CheckState.Checked;
+            checkBoxUseDither.Location = new Point(176, 62);
+            checkBoxUseDither.Name = "checkBoxUseDither";
+            checkBoxUseDither.Size = new Size(96, 19);
+            checkBoxUseDither.TabIndex = 14;
+            checkBoxUseDither.Text = "Use dithering";
+            checkBoxUseDither.UseVisualStyleBackColor = true;
+            checkBoxUseDither.CheckedChanged += checkBoxUseDither_CheckedChanged;
+            // 
+            // comboBoxDistance
+            // 
+            comboBoxDistance.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBoxDistance.FormattingEnabled = true;
+            comboBoxDistance.Items.AddRange(new object[] { "difference", "RGB euclid", "YUV euclid" });
+            comboBoxDistance.Location = new Point(176, 149);
+            comboBoxDistance.Name = "comboBoxDistance";
+            comboBoxDistance.Size = new Size(96, 23);
+            comboBoxDistance.TabIndex = 15;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(172, 123);
+            label1.Name = "label1";
+            label1.Size = new Size(100, 15);
+            label1.TabIndex = 16;
+            label1.Text = "Distance method:";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(703, 536);
-            Controls.Add(checkBox1);
+            ClientSize = new Size(900, 536);
+            Controls.Add(label1);
+            Controls.Add(comboBoxDistance);
+            Controls.Add(checkBoxUseDither);
+            Controls.Add(comboBoxDither);
+            Controls.Add(checkBoxSimpleAvg);
+            Controls.Add(pictureBox8);
+            Controls.Add(button2);
+            Controls.Add(pictureBox7);
+            Controls.Add(pictureBox6);
+            Controls.Add(pictureBox5);
+            Controls.Add(checkBoxInverse);
             Controls.Add(pictureBox4);
             Controls.Add(pictureBox3);
             Controls.Add(pictureBox2);
@@ -113,6 +234,10 @@
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -124,6 +249,16 @@
         private PictureBox pictureBox2;
         private PictureBox pictureBox3;
         private PictureBox pictureBox4;
-        private CheckBox checkBox1;
+        private CheckBox checkBoxInverse;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox7;
+        private Button button2;
+        private PictureBox pictureBox8;
+        private CheckBox checkBoxSimpleAvg;
+        private ComboBox comboBoxDither;
+        private CheckBox checkBoxUseDither;
+        private ComboBox comboBoxDistance;
+        private Label label1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,8 +1,9 @@
 namespace AlterLinePictureAproximator
 {
-    using ColorSpace;
+    using System.CodeDom;
     using System.Drawing;
     using System.Numerics;
+    using System.Text.Json.Serialization.Metadata;
 
     public partial class Form1 : Form
     {
@@ -20,6 +21,7 @@ namespace AlterLinePictureAproximator
         public byte[] line1 = new byte[5] { 0x14, 0x16, 0x48, 0xb6, 0x00 };
         public Color[] cline0 = new Color[4];
         public Color[] cline1 = new Color[4];
+        public int[,,] diff_matrix;
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -27,36 +29,40 @@ namespace AlterLinePictureAproximator
             Vector4 lab = Conversion.RGBToLab(rgb);
             Vector4 rgb2 = Conversion.LabToRGB(lab);*/
             LoadPalette();
-            CreatePal(false);
+            CreatePal(false, false);
         }
 
-        private Color AverageColor(int c1, int c2)
+        private Color AverageColor(Color c1, Color c2, bool simple)
         {
-            double r2 = (Math.Pow((int)atariPalRgb[c1 * 3], 2) + Math.Pow((int)atariPalRgb[c2 * 3], 2)) / 2;
-            double g2 = (Math.Pow((int)atariPalRgb[c1 * 3 + 1], 2) + Math.Pow((int)atariPalRgb[c2 * 3 + 1], 2)) / 2;
-            double b2 = (Math.Pow((int)atariPalRgb[c1 * 3 + 2], 2) + Math.Pow((int)atariPalRgb[c2 * 3 + 2], 2)) / 2;
-            return Color.FromArgb((int)double.Floor(Math.Sqrt(r2)), (int)double.Floor(Math.Sqrt(g2)), (int)double.Floor(Math.Sqrt(b2)));
+            if (!simple)
+            {
+                int r2 = (c1.R * c1.R + c2.R * c2.R) / 2;
+                int g2 = (c1.G * c1.G + c2.G * c2.G) / 2;
+                int b2 = (c1.B * c1.B + c2.B * c2.B) / 2;
+                return Color.FromArgb(Clamp256((int)Math.Sqrt(r2)), Clamp256((int)Math.Sqrt(g2)), Clamp256((int)Math.Sqrt(b2)));
+            }
 
-
-            /*Vector4 cv1 = Rgb2Vector(atariPalRgb[c1 * 3], atariPalRgb[c1 * 3+1], atariPalRgb[c1 * 3+2]);
-            Vector4 cv2 = Rgb2Vector(atariPalRgb[c2 * 3], atariPalRgb[c2 * 3 + 1], atariPalRgb[c2 * 3 + 2]);
-            Vector4 cl1 = ColorSpace.Conversion.RGBToLab(cv1);
-            Vector4 cl2 = ColorSpace.Conversion.RGBToLab(cv2);
-            Vector4 nc = new Vector4((cl1.X+cl2.X)/2,(cl1.Y+cl2.Y)/2,(cl1.Z+cl2.Z)/2,0);
-            Vector4 ncrgb = ColorSpace.Conversion.LabToRGB(cl1); // nc);
-
-            return Color.FromArgb((int)(ncrgb.X * 255), (int)(ncrgb.Y * 255), (int)(ncrgb.Z * 255));
-            */
-            /*
+            int r = (c1.R + c2.R) / 2;
+            int g = (c1.G + c2.G) / 2;
+            int b = (c1.B + c2.B) / 2;
+            return Color.FromArgb(r, g, b);
+        }
+        private Color AverageColor(int c1, int c2, bool simple)
+        {
+            if (!simple)
+            {
+                double r2 = (Math.Pow((int)atariPalRgb[c1 * 3], 2) + Math.Pow((int)atariPalRgb[c2 * 3], 2)) / 2;
+                double g2 = (Math.Pow((int)atariPalRgb[c1 * 3 + 1], 2) + Math.Pow((int)atariPalRgb[c2 * 3 + 1], 2)) / 2;
+                double b2 = (Math.Pow((int)atariPalRgb[c1 * 3 + 2], 2) + Math.Pow((int)atariPalRgb[c2 * 3 + 2], 2)) / 2;
+                return Color.FromArgb(Clamp256((int)Math.Sqrt(r2)), Clamp256((int)Math.Sqrt(g2)), Clamp256((int)Math.Sqrt(b2)));
+            }
             int r = ((int)atariPalRgb[c1 * 3] + (int)atariPalRgb[c2 * 3]) / 2;
             int g = ((int)atariPalRgb[c1 * 3 + 1] + (int)atariPalRgb[c2 * 3 + 1]) / 2;
             int b = ((int)atariPalRgb[c1 * 3 + 2] + (int)atariPalRgb[c2 * 3 + 2]) / 2;
-
-            //return Color.FromArgb(Math.Abs(atariPalRgb[c1] - atariPalRgb[c2]), Math.Abs(atariPalRgb[c1 + 256] - atariPalRgb[c2 + 256]), Math.Abs(atariPalRgb[c1 + 2 * 256] - atariPalRgb[c2 + 2 * 256]));
-            return Color.FromArgb(r, g, b);*/
+            return Color.FromArgb(r, g, b);
         }
 
-        private void CreatePal(bool inverse)
+        private void CreatePal(bool inverse, bool simple)
         {
             Bitmap p = new Bitmap(128, 200);
             Graphics g = Graphics.FromImage(p);
@@ -71,11 +77,11 @@ namespace AlterLinePictureAproximator
             {
                 for (int b = 0; b < 4; b++)
                 {
-                    customColors[a * 4 + b] = AverageColor(l0[a], l1[b]);
+                    customColors[a * 4 + b] = AverageColor(l0[a], l1[b], simple);
                     g.FillRectangle(new SolidBrush(customColors[a * 4 + b]), new Rectangle(b * 32, a * 32, 32, 32));
                 }
-                cline0[a] = AverageColor(l0[a], l0[a]);
-                cline1[a] = AverageColor(l1[a], l1[a]);
+                cline0[a] = AverageColor(l0[a], l0[a], simple);
+                cline1[a] = AverageColor(l1[a], l1[a], simple);
                 g.FillRectangle(new SolidBrush(cline0[a]), new Rectangle(a * 32, 128 + 8, 32, 32));
                 g.FillRectangle(new SolidBrush(cline1[a]), new Rectangle(a * 32, 128 + 32 + 8, 32, 32));
             }
@@ -89,7 +95,7 @@ namespace AlterLinePictureAproximator
 
         private void button1_Click(object sender, EventArgs e)
         {
-            DoConvert();
+            DoConvert(checkBoxSimpleAvg.Checked, comboBoxDistance.SelectedIndex, checkBoxUseDither.Checked, comboBoxDither.SelectedIndex);
         }
 
         public static int Clamp(int value, int min, int max)
@@ -101,85 +107,122 @@ namespace AlterLinePictureAproximator
         {
             return (value < 0) ? 0 : (value > 255) ? 255 : value;
         }
-        private void DoConvert()
+        private void DoConvert(bool simple, int distanceMethod, bool dither, int ditherMethod = 0)
         {
             Bitmap b = (Bitmap)pictureBox1.Image;
             Bitmap t = new Bitmap(b.Width * 2, b.Height);
             Bitmap a = new Bitmap(b.Width * 2, b.Height);
             int[,,] delta_matrix = new int[b.Width + 1, b.Height + 1, 3];
+            diff_matrix = new int[b.Width + 1, b.Height + 1, 2];
 
-            for (int y = 0; y < pictureBox1.Image.Height / 2; y++)
+            for (int z = 0; z < 2; z++)
             {
-                for (int x = 1; x < pictureBox1.Image.Width - 1; x++)
+                CreatePal(z == 1, simple);
+                delta_matrix.Initialize();
+                for (int y = 0; y < pictureBox1.Image.Height / 2; y++)
                 {
-                    Color src = b.GetPixel(x, y * 2);
-                    int[] channel = new int[3]{src.R + delta_matrix[x,y,0],
+                    for (int x = 1; x < pictureBox1.Image.Width - 1; x++)
+                    {
+                        Color src = AverageColor(b.GetPixel(x, y * 2), b.GetPixel(x, y * 2 + 1), simple);
+                        int[] channel = new int[3]{src.R + delta_matrix[x,y,0],
                                                src.G + delta_matrix[x,y,1],
                                                src.B + delta_matrix[x,y,2]};
-                    Color srcPlusDelta = Color.FromArgb(Clamp256(channel[0]), Clamp256(channel[1]), Clamp256(channel[2]));
-                    int index = FindCustomClosest2(srcPlusDelta);   //use "src" to disable error diffusion
-                    // 1/2*|# 1|
-                    //     |1 0|
+                        Color srcPlusDelta = Color.FromArgb(Clamp256(channel[0]), Clamp256(channel[1]), Clamp256(channel[2]));
+                        int index = FindCustomClosest2(dither ? srcPlusDelta : src, distanceMethod);   //use "src" to disable error diffusion
+                        diff_matrix[x, y, z] = (int)Distance(srcPlusDelta, customColors[index], distanceMethod);
+                        // 1/2*|# 1|
+                        //     |1 0|
 
-                    // 1/4*|- # 2|
-                    //     |1 1 0|
+                        // 1/4*|- # 2|
+                        //     |1 1 0|
 
-                    // 1/16*|- # 7|
-                    //      |3 5 1|
-                    int[] delta = new int[3] { src.R - customColors[index].R + (channel[0] - Clamp256(channel[0])),
+                        // 1/16*|- # 7|
+                        //      |3 5 1|
+                        int[] delta = new int[3] { src.R - customColors[index].R + (channel[0] - Clamp256(channel[0])),
                                                src.G - customColors[index].G + (channel[1] - Clamp256(channel[1])),
                                                src.B - customColors[index].B + (channel[2] - Clamp256(channel[2]))};
-                    for (int i = 0; i < 3; i++)
-                    {
-                        //chess
-                        if ((x + y) % 2 != 0)
+                        for (int i = 0; i < 3; i++)
                         {
-                            delta_matrix[x + 1, y, i] = delta[i] / 2;
-                            delta_matrix[x, y + 1, i] = delta[i] / 2;
+                            switch (ditherMethod)   //chess, sierra, F-S
+                            {
+                                case 0: //chess
+                                    if ((x + y) % 2 == 0)
+                                    {
+                                        delta_matrix[x + 1, y, i] = delta[i] / 2;
+                                        delta_matrix[x, y + 1, i] = delta[i] / 2;
+                                    }
+                                    break;
+                                case 1: //sierra
+                                    delta_matrix[x + 1, y, i] = delta[i] / 2;
+                                    delta_matrix[x + 1, y + 1, i] = delta[i] / 4;
+                                    delta_matrix[x, y + 1, i] = delta[i] / 4;
+                                    break;
+                                case 2: //f-s
+                                    delta_matrix[x + 1, y, i] = (int)float.Floor((delta[i] / 16.0f) * 7);
+                                    delta_matrix[x - 1, y + 1, i] = (int)float.Floor((delta[i] / 16.0f) * 3);
+                                    delta_matrix[x, y + 1, i] = (int)float.Floor((delta[i] / 16.0f) * 5);
+                                    delta_matrix[x + 1, y + 1, i] = (int)float.Floor((delta[i] / 16.0f) * 1);
+                                    break;
+                                default:
+                                    throw new NotImplementedException();
+                            }
+
                         }
-                        /*delta_matrix[x + 1,y  ,i] = delta[i] / 2;
-                        delta_matrix[x + 1,y+1,i] = delta[i]/4 ;
-                        delta_matrix[x    ,y+1,i] = delta[i] / 4;*/
 
-                        //f-s
-                        delta_matrix[x + 1, y, i] = (int)float.Floor((delta[i] / 16.0f) * 7);
-                        delta_matrix[x - 1, y + 1, i] = (int)float.Floor((delta[i] / 16.0f) * 3);
-                        delta_matrix[x, y + 1, i] = (int)float.Floor((delta[i] / 16.0f) * 5);
-                        delta_matrix[x + 1, y + 1, i] = (int)float.Floor((delta[i] / 16.0f) * 1);
+                        Color p1 = cline0[index / 4];
+                        Color p2 = cline1[index % 4];
+
+                        a.SetPixel(x * 2, y * 2, p1);
+                        a.SetPixel(x * 2 + 1, y * 2, p1);
+                        a.SetPixel(x * 2, y * 2 + 1, p2);
+                        a.SetPixel(x * 2 + 1, y * 2 + 1, p2);
+
+                        t.SetPixel(x * 2, y * 2, customColors[index]);
+                        t.SetPixel(x * 2 + 1, y * 2, customColors[index]);
+                        t.SetPixel(x * 2, y * 2 + 1, customColors[index]);
+                        t.SetPixel(x * 2 + 1, y * 2 + 1, customColors[index]);
                     }
-
-                    Color p1 = cline0[index / 4];
-                    Color p2 = cline1[index % 4];
-
-                    a.SetPixel(x * 2, y * 2, p1);
-                    a.SetPixel(x * 2 + 1, y * 2, p1);
-                    a.SetPixel(x * 2, y * 2 + 1, p2);
-                    a.SetPixel(x * 2 + 1, y * 2 + 1, p2);
-
-                    t.SetPixel(x * 2, y * 2, customColors[index]);
-                    t.SetPixel(x * 2 + 1, y * 2, customColors[index]);
-                    t.SetPixel(x * 2, y * 2 + 1, customColors[index]);
-                    t.SetPixel(x * 2 + 1, y * 2 + 1, customColors[index]);
                 }
-                /*for (int x = 1; x < pictureBox1.Image.Width - 1; x++)
+                if (z == 0)
                 {
-                    for (int i = 0; i < 3; i++)
-                        delta_line[x, i] = 0;
-                }*/
+                    pictureBox2.Image = new Bitmap(t);
+                    pictureBox3.Image = new Bitmap(a);
+                }
+                else
+                {
+                    pictureBox5.Image = new Bitmap(t);
+                    pictureBox6.Image = new Bitmap(a);
+                }
             }
-            pictureBox2.Image = t;
-            pictureBox3.Image = a;
         }
 
-
-        private int FindCustomClosest2(Color color)
+        private double Distance(Color col1, Color col2, int distanceMethod)
+        {
+            double dist = double.MaxValue;
+            switch (distanceMethod)
+            {
+                case 0:
+                    dist = Difference(col1, col2);
+                    break;
+                case 1:
+                    dist = RGBEuclidianDistance(col1, col2);
+                    break;
+                case 2:
+                    dist = RGByuvDistance(col1, col2);
+                    break;
+                default:
+                    throw new NotImplementedException();
+                    break;
+            }
+            return dist;
+        }
+        private int FindCustomClosest2(Color color, int distanceMethod)
         {
             int index = 0;
             double maxdist = double.MaxValue;
             for (int i = 0; i < 16; i++)
             {
-                double dist = RGByuvDistance(color, customColors[i]);
-
+                double dist = Distance(color, customColors[i], distanceMethod);
                 if (dist < maxdist)
                 {
                     maxdist = dist;
@@ -189,9 +232,12 @@ namespace AlterLinePictureAproximator
             return index;
         }
 
-        private static double Difference(Vector4 vct1, Vector4 vct2)
+        private static double Difference(Color col1, Color col2)
         {
-            return Math.Sqrt(Math.Pow(vct1.X - vct2.X, 2) + Math.Pow(vct1.Y - vct2.Y, 2) + Math.Pow(vct1.Z - vct2.Z, 2));
+            int dr = col2.R - col1.R;
+            int dg = col2.G - col1.G;
+            int db = col2.B - col1.B;
+            return Math.Abs(dr) + Math.Abs(dg) + Math.Abs(db);
         }
 
         private float RGByuvDistance(Color col1, Color col2)
@@ -232,7 +278,51 @@ namespace AlterLinePictureAproximator
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
-            CreatePal(checkBox1.Checked);
+            CreatePal(checkBoxInverse.Checked, checkBoxSimpleAvg.Checked);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MixIt();
+        }
+
+        private void MixIt()
+        {
+            Bitmap b = new Bitmap(pictureBox3.Image); //non inverse
+            Bitmap m = new Bitmap(pictureBox3.Image);
+            Graphics g = Graphics.FromImage(b);
+            Graphics gm = Graphics.FromImage(m);
+
+            double[,,] chardiff = new double[32, 16, 2];
+            for (int y = 0; y < 16; y++)
+                for (int x = 0; x < 32; x++)
+                {
+                    for (int z = 0; z < 2; z++)
+                        for (int cy = 0; cy < 4; cy++)
+                            for (int cx = 0; cx < 4; cx++)
+                                chardiff[x, y, z] += Math.Pow(diff_matrix[x * 4 + cx, y * 4 + cy, z], 2);
+                    if (chardiff[x, y, 0] > chardiff[x, y, 1])
+                    {
+                        g.DrawImage(pictureBox6.Image/*inverse one*/, x * 8, y * 8, new Rectangle(x * 8, y * 8, 8, 8), GraphicsUnit.Pixel);
+                        gm.FillRectangle(new SolidBrush(Color.Black), new Rectangle(x * 8, y * 8, 8, 8));
+                    }
+                    else
+                    {
+                        gm.FillRectangle(new SolidBrush(Color.White), new Rectangle(x * 8, y * 8, 8, 8));
+                    }
+                }
+            pictureBox7.Image = b;
+            pictureBox8.Image = m;
+        }
+
+        private void checkBoxSimple_CheckedChanged(object sender, EventArgs e)
+        {
+            CreatePal(checkBoxInverse.Checked, checkBoxSimpleAvg.Checked);
+        }
+
+        private void checkBoxUseDither_CheckedChanged(object sender, EventArgs e)
+        {
+            comboBoxDither.Enabled = checkBoxUseDither.Checked;
         }
     }
 }
